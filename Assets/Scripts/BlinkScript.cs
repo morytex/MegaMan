baf66fa76f3ddcaad2dfedcc43a96156af538df3
logonrm <logonrm@fiap.com.br>
2018-02-05 22:44:53 -0200

    Adicionando cena nova e transicao

--- Assets/Scripts/BlinkScript.cs
+++ Assets/Scripts/BlinkScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BlinkScript : MonoBehaviour {
 
@@ -14,5 +15,9 @@ public class BlinkScript : MonoBehaviour {
 		StartCoroutine (Start ());
 	}
 	
-
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			SceneManager.LoadScene ("GameScene");
+		}
+	}
 }
