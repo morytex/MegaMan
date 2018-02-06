5c92b7270707dc86b6e6ecc4c4b124aaf606ca98
logonrm <logonrm@fiap.com.br>
2018-02-05 22:25:43 -0200

    Aplicando blink no texto de introducao

---
+++ Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScript : MonoBehaviour {
+
+	public float intervalo;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		GetComponent<SpriteRenderer> ().enabled = 
+			!GetComponent<SpriteRenderer> ().enabled;
+		yield return new WaitForSeconds(intervalo);
+		StartCoroutine (Start ());
+	}
+	
+
+}
